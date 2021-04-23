f81336fde8811080e2b5cd1de3b49ca9274c160d
Corentin Cadiou <corentin.cadiou@cphyc.me>
2021-04-23 09:40:44 +0100

    Move to more modern infra

--- setup.py
+++ setup.py
@@ -1,6 +1,4 @@
-# -*- coding: utf-8 -*-
-
-from setuptools import setup, find_packages
+from setuptools import setup
 from setuptools.extension import Extension
 from Cython.Build import cythonize
 
@@ -20,35 +18,6 @@ cython_extensions = [
               include_dirs=include_dirs)
 ]
 
-
-# with open('Readme.md') as f:
-#     readme = f.read()
-
-# with open('LICENSE') as f:
-#     license = f.read()
-
 setup(
-    name='correlations',
-    version='0.0.1',
-    description='Compute correlations functions for Gaussian Random Fields.',
-    classifiers=[
-        'Development status :: 1 - Alpha',
-        'License :: CC-By-SA2.0',
-        'Programming Language :: Python',
-        'Topic :: Gaussian Random Field'
-    ],
-    author='Corentin Cadiou',
-    author_email='contact@cphyc.me',
-    packages=['correlations'],
-    package_dir={'correlations': 'correlations'},
-    package_data={'correlations': [
-        'data/power.dat',
-        'correlations/*.pyx'
-    ]},
-    install_requires=[
-        'numpy',
-        'mezmorize>=0.25.0',
-    ],
-    include_package_data=True,
     ext_modules=cythonize(cython_extensions)
 )
