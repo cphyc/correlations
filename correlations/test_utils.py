3105dd5e3c3f39f68089c18192034bf3da702a94
Corentin Cadiou <corentin.cadiou@cphyc.me>
2023-02-22 10:40:51 -0800

    Test utils ftw

--- correlations/test_utils.py
+++ correlations/test_utils.py
@@ -1,6 +1,8 @@
 from pathlib import Path
+import pytest
 
 import numpy as np
+import scipy
 from scipy.integrate import dblquad, nquad, tplquad
 
 from correlations.correlations import integrand_python
@@ -49,57 +51,72 @@ def test_integrand():
         check_it(theta, phi)
 
 
-def test_integration():
-    ikx, iky, ikz, ikk = 0, 0, 1, 2  # This is chosen randomly
-    xyz = np.array([0, 0, 0])
+ikx, iky, ikz, ikk = 0, 0, 1, 2  # This is chosen randomly
+xyz = np.array([0, 0, 0])
 
-    def test_x(x, ikx, ikk, iky):
-        xyz[0] = x
+combinations = []
+for x in np.linspace(0, 2, 5):
+    for ikk in [2, 0]:
+        for ikx in [0, 1, 2]:
+            for iky in [0, 1]:
+                xyz[0] = x
+                combinations.append((x, ikx, ikk, iky, ikz, xyz.copy()))
 
-        ref = dblquad(
-            integrand_python,
-            0,
-            np.pi,
-            lambda _: 0,
-            lambda _: 2 * np.pi,
-            args=(ikx, iky, ikz, ikk, *xyz, 1, 1),
-        )[0]
 
-        def do_integration(x, ikx, iky, ikk, integrator, integrand):
-            a, da = integrator(integrand, *bounds, **kwa)
-            print(f"expected {ref}, got {a}")
-            np.testing.assert_allclose(a, ref, atol=1e-10)
+@pytest.mark.parametrize("combinations", combinations)
+def test_integration(combinations, recwarn):
+    x, ikx, ikk, iky, ikz, xyz = combinations
 
-        kwa = {"epsrel": 1e-5, "args": (ikx, iky, ikz, ikk, *list(xyz), 1, 1)}
+    ref = dblquad(
+        integrand_python,
+        0,
+        np.pi,
+        lambda _: 0,
+        lambda _: 2 * np.pi,
+        args=(ikx, iky, ikz, ikk, *xyz, 1, 1),
+    )[0]
 
-        bounds = (0, np.pi, lambda _: 0, lambda _: 2 * np.pi)
-        do_integration(x, ikx, iky, ikk, dblquad, u.integrand_lambdaCDM)
+    def do_integration(x, ikx, iky, ikk, integrator, integrand):
+        print(f"Testing {integrator.__name__} for x={x} ikx={ikx} ikk={ikk} iky={iky}")
+        a, da = integrator(integrand, *bounds, **kwa)
+        np.testing.assert_allclose(a, ref, atol=1e-10)
 
-        bounds = (
-            0,
-            np.pi,
-            lambda _: 0,
-            lambda _: 2 * np.pi,
-            lambda _1, _2: 0,
-            lambda _1, _2: np.inf,
-        )
-        do_integration(x, ikx, iky, ikk, tplquad, u.integrand)
+    kwa = {"epsrel": 1e-5, "args": (ikx, iky, ikz, ikk, *list(xyz), 1, 1)}
 
-        kwa = {
-            "opts": [
-                {"epsrel": 1e-6},  # k integral
-                {"epsrel": 1e-6},  # phi integral
-                {"epsrel": 1e-6},  # theta integral
-            ],
-            "args": (ikx, iky, ikz, ikk, *list(xyz), 1, 1),
-        }
+    bounds = (0, np.pi, lambda _: 0, lambda _: 2 * np.pi)
+    do_integration(x, ikx, iky, ikk, dblquad, u.integrand_lambdaCDM)
 
-        # Note: the integration order is the opposite for nquad...
-        bounds = ([(0, np.inf), (0, 2 * np.pi), (0, np.pi)],)
-        do_integration(x, ikx, iky, ikk, nquad, u.integrand)
+    bounds = (
+        0,
+        np.pi,
+        lambda _: 0,
+        lambda _: 2 * np.pi,
+        lambda _1, _2: 0,
+        lambda _1, _2: np.inf,
+    )
 
-    for x in np.linspace(0, 2, 5):
-        for ikk in [2, 0]:
-            for ikx in [0, 1, 2]:
-                for iky in [0, 1]:
-                    test_x(x, ikx, ikk, iky)
+    def tplquad_no_warning(*args, **kwargs):
+        # There may be a warning about roundoff error, but we don't care
+        recwarn.clear()
+        out = tplquad(*args, **kwargs)
+
+        while len(recwarn) > 0:
+            w = recwarn.pop()
+            assert w.category == scipy.integrate.IntegrationWarning
+
+        return out
+
+    do_integration(x, ikx, iky, ikk, tplquad_no_warning, u.integrand)
+
+    kwa = {
+        "opts": [
+            {"epsrel": 1e-6},  # k integral
+            {"epsrel": 1e-6},  # phi integral
+            {"epsrel": 1e-6},  # theta integral
+        ],
+        "args": (ikx, iky, ikz, ikk, *list(xyz), 1, 1),
+    }
+
+    # Note: the integration order is the opposite for nquad...
+    bounds = ([(0, np.inf), (0, 2 * np.pi), (0, np.pi)],)
+    do_integration(x, ikx, iky, ikk, nquad, u.integrand)
